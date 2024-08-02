4490b0caff63eae249c0183954872c03eb781da8
larry <1762861715@qq.com>
2024-08-02 23:27:13 +0800

    2

--- src/check.ts
+++ src/check.ts
@@ -1,13 +1,25 @@
 import * as vscode from 'vscode';
 const checkExtension = async () => {
-    const env = vscode.extensions.getExtension('oracle.oracle-java')
-    if (!env) {
-        const select = await vscode.window.showInformationMessage('检测到当前无Java相关插件,推荐安装', '确定','取消')
-        if (select === '确定') {
-            vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-java-pack');
+    let pom = vscode.workspace.findFiles('**/pom.xml');
+    if (pom !== undefined) {
+        const env = vscode.extensions.getExtension('vscjava.vscode-maven');
+        if (!env) {
+            const select = await vscode.window.showInformationMessage('检测到当前无Maven相关插件,推荐安装', '确定', '取消');
+            if (select === '确定') {
+                vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-java-pack');
+            }
+        }
+    } else {
+        const env = vscode.extensions.getExtension('vscjava.vscode-gradle');
+        if (!env) {
+            const select = await vscode.window.showInformationMessage('检测到当前无Gradle相关插件,推荐安装', '确定', '取消');
+            if (select === '确定') {
+                vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-gradle');
+            }
         }
     }
-    
-}
 
-export { checkExtension }
+
+};
+
+export { checkExtension };
