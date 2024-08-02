881724516fbc53a48e67ba6f3cd8eb284aa62d90
larry <1762861715@qq.com>
2024-08-03 02:32:19 +0800

    4

--- src/check.ts
+++ src/check.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
 const checkExtension = async () => {
-    let pom = vscode.workspace.findFiles('**/pom.xml');
-    if (pom !== undefined) {
+    let pom = await vscode.workspace.findFiles('**/pom.xml');
+    if (pom && pom.length > 0) {
         const env = vscode.extensions.getExtension('vscjava.vscode-maven');
         if (!env) {
-            const select = await vscode.window.showInformationMessage('检测到当前无Maven相关插件,推荐安装', '确定', '取消');
+            const select =  await vscode.window.showInformationMessage('检测到当前无Maven相关插件,推荐安装', '确定', '取消');
             if (select === '确定') {
                 vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-java-pack');
             }
