10d572e95e141663c663a21357ac1bb36d5c9d69
larry <1762861715@qq.com>
2024-08-03 15:49:26 +0800

    5

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,9 @@
 import * as vscode from 'vscode';
-
+import * as tool from './tool';
 const initStartButton = (context: vscode.ExtensionContext) => {
-    context.subscriptions.push(vscode.commands.registerCommand('solon.helper.run', async () => {
-        vscode.window.showInformationMessage('run');
-    }))
-}
+    context.subscriptions.push(vscode.commands.registerCommand('solon.run', async () => {
+        // 默认的java插件自带
+    }));
+};
 
-export { initStartButton }
+export { initStartButton };
