ee4af7d0fc47741bd6f0bf93d1122eb3195691bd
larry <1762861715@qq.com>
2024-08-06 01:44:13 +0800

    1

--- src/check.ts
+++ src/check.ts
@@ -1,23 +1,25 @@
 import * as vscode from 'vscode';
 const checkExtension = async () => {
-    let pom = await vscode.workspace.findFiles('**/pom.xml');
-    if (pom && pom.length > 0) {
-        const env = vscode.extensions.getExtension('vscjava.vscode-maven');
-        if (!env) {
-            const select =  await vscode.window.showInformationMessage('检测到当前无Maven相关插件,推荐安装', '确定', '取消');
-            if (select === '确定') {
-                vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-java-pack');
+    setTimeout(async () => {
+        let pom = await vscode.workspace.findFiles('**/pom.xml');
+        if (pom && pom.length > 0) {
+            const env = vscode.extensions.getExtension('vscjava.vscode-maven');
+            if (!env) {
+                const select = await vscode.window.showInformationMessage('检测到当前无Maven相关插件,推荐安装', '确定', '取消');
+                if (select === '确定') {
+                    vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-java-pack');
+                }
+            }
+        } else {
+            const env = vscode.extensions.getExtension('vscjava.vscode-gradle');
+            if (!env) {
+                const select = await vscode.window.showInformationMessage('检测到当前无Gradle相关插件,推荐安装', '确定', '取消');
+                if (select === '确定') {
+                    vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-gradle');
+                }
             }
         }
-    } else {
-        const env = vscode.extensions.getExtension('vscjava.vscode-gradle');
-        if (!env) {
-            const select = await vscode.window.showInformationMessage('检测到当前无Gradle相关插件,推荐安装', '确定', '取消');
-            if (select === '确定') {
-                vscode.commands.executeCommand('workbench.extensions.search', 'vscjava.vscode-gradle');
-            }
-        }
-    }
+    }, 10000);
 
 
 };
