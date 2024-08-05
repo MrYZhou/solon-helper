1743c9792752fa0ffa2c3cf618c0f3bca9ec063a
larry <1762861715@qq.com>
2024-08-06 00:30:21 +0800

    prefix解析

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,13 @@
 import * as vscode from 'vscode';
 import { checkExtension } from './check';
 import { initInitializr } from './initializr';
-import { initStartButton } from './start';
 import { initYmlSuggestion } from './yml';
 
 
 
 export function activate(context: vscode.ExtensionContext) {
     checkExtension();
-    initInitializr(context);
-    initStartButton(context);
     initYmlSuggestion(context);
+    initInitializr(context);
 }
 export function deactivate() { }
