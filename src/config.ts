255c801e3c4b39fcc6946f2b856c23140b8ab580
larry <1762861715@qq.com>
2024-08-08 14:47:56 +0800

    quickpath

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as os from 'os';
+const initConfig = (context: vscode.ExtensionContext) => {
+    const configuration = vscode.workspace.getConfiguration('solon-helper');
+    let customPath: any = configuration.inspect('customPath');
+    if (!customPath?.globalValue) {
+        configuration.update('customPath', {
+            '桌面': path.join(os.homedir(), 'Desktop')
+        }, vscode.ConfigurationTarget.Global);
+    }
+};
+
+export { initConfig };
