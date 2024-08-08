4898fc16473046fc0d1484a72a0d2022c8f03e57
larry <1762861715@qq.com>
2024-08-08 14:47:56 +0800

    quickpath

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,12 @@ import * as vscode from 'vscode';
 import { checkExtension } from './check';
 import { initInitializr } from './initializr';
 import { initYmlSuggestion } from './yml';
-
+import {initConfig} from './config';
 
 
 export function activate(context: vscode.ExtensionContext) {
     checkExtension();
+    initConfig(context);
     initYmlSuggestion(context);
     initInitializr(context);
 }
